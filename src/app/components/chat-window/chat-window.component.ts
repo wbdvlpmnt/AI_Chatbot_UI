4448da63e655ca9d029522e68694608978c1ca8f
wbdvlpmnt <jai.gokhale5219@gmail.com>
2024-07-25 10:48:10 -0500

    scaffold messaging

--- src/app/components/chat-window/chat-window.component.ts
+++ src/app/components/chat-window/chat-window.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+type message = {
+  messageType: 'system' | 'user';
+  messageContent: string;
+};
+
 @Component({
   selector: 'app-chat-window',
   templateUrl: './chat-window.component.html',
-  styleUrls: ['./chat-window.component.scss']
+  styleUrls: ['./chat-window.component.scss'],
 })
 export class ChatWindowComponent {
-
+  messages: message[] = [
+    {
+      messageType: 'system',
+      messageContent: 'How may I help you today?',
+    },
+    {
+      messageType: 'user',
+      messageContent: 'what is the larget mountain?',
+    },
+  ];
 }
